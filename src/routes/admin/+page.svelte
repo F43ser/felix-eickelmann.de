a1f824acdad13bfdf50fbf97e4d0a1a17057e29a
Felix Eickelmann <kontakt@felix-eickelmann.de>
2026-06-05 17:18:20 +0200

    created repo

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>Admin-Dashboard</title>
+	<script src="https://unpkg.com/@sveltia/cms/dist/sveltia-cms.js" type="module"></script>
+</svelte:head>
+
+<div>Lädt Admin-Oberfläche...</div>
